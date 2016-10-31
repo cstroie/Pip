9af4f2883739cd1c887d33bc222787588ce9c0c7
Costin STROIE <cstroie@bitdefender.com>
2016-10-31 17:54:01 +0200

    Some new config.

--- config.lua
+++ config.lua
@@ -10,11 +10,12 @@ lcd_id  = 0
 lcd_sda = 1      -- GPIO5
 lcd_scl = 2      -- GPIO4
 lcd_dev = 0x27   -- I2C Address
+lcd_bl  = true   -- Backlight
 
 -- NTP server
 ntp_server = "0.europe.pool.ntp.org"
 ntp_interval = "765"
-timezone = 3
+timezone = 2
 
 -- DHT pin
 dht11_pin = 4
@@ -29,5 +30,18 @@ iot_port = 17223
 iot_id = "MQTT_ID"
 iot_user = "MQTT_USER"
 iot_pass = "MQTT_PASS"
+
+-- Beep
+beep_pin = 3
+beep_pls = 500
+beep_rpt = 100
+
+-- RCS
+rcs_pin = 0
+rcs_pl = 255
+rcs_bits = 24
+rcs_proto = 1
+rcs_count = 4
+rcs_dip = 0x1f
 
 -- vim: set ft=lua ai ts=2 sts=2 et sw=2 sta nowrap nu :
