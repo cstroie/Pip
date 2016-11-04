8189566a57342f74ed301eee96b81527b30cc5dc
Costin STROIE <cstroie@bitdefender.com>
2016-11-04 17:27:09 +0200

    Better dispatcher.

--- rcs.lua
+++ rcs.lua
@@ -3,41 +3,38 @@
 require("config")
 
 local rcs = {}
-rcs.sw = {0x004000, 0x010000, 0x040000, 0x100000, 0x400000}
+rcs.SWITCHES = {0x004000, 0x010000, 0x040000, 0x100000, 0x400000}
+rcs.ON  = {A = 0x000551, B = 0x001151, C = 0x001451, D = 0x001511}
+rcs.OFF = {A = 0x000554, B = 0x001154, C = 0x001454, D = 0x00155f}
+
+function rcs:send(bits)
+  rc.send(rcs_pin, bits, rcs_bits, rcs_pl, rcs_proto, rcs_count)
+end
 
 function rcs:switch(dip)
   local result = 0x00
-  for k, v in pairs(self.sw) do
+  for k, v in ipairs(self.SWITCHES) do
     if not bit.isset(dip, k - 1) then result = result + v end
   end
   return result
 end
 
 function rcs:on(btn, dip)
-  if dip == nil then dip = rcs_dip end
-  local code = 0x000000
-  if     btn == "A" then code = 0x000551
-  elseif btn == "B" then code = 0x001151
-  elseif btn == "C" then code = 0x001451
-  elseif btn == "D" then code = 0x001511 end
-  rc.send(rcs_pin, code + self:switch(dip), rcs_bits, rcs_pl, rcs_proto, rcs_count)
+  self:button(btn, "ON", dip)
 end
 
 function rcs:off(btn, dip)
-  if dip == nil then dip = rcs_dip end
-  local code = 0x000000
-  if     btn == "A" then code = 0x000554
-  elseif btn == "B" then code = 0x001154
-  elseif btn == "C" then code = 0x001454
-  elseif btn == "D" then code = 0x00155f end
-  rc.send(rcs_pin, code + self:switch(dip), rcs_bits, rcs_pl, rcs_proto, rcs_count)
+  self:button(btn, "OFF", dip)
 end
 
-function rcs:button(btn, cmd)
+function rcs:button(btn, cmd, dip)
+  -- Send the command to the button
   btn = btn:upper()
-  if DEBUG then print("RCS " .. btn .. ": " .. cmd) end
-  if cmd == "on" then self:on(btn)
-  elseif cmd == "off" then self:off(btn) end
+  cmd = cmd:upper()
+  dip = dip or rcs_dip
+  debug("RCS " .. btn .. ": " .. cmd)
+  local code = rcs[cmd][btn] or 0x000000
+  self:send(code + self:switch(dip))
 end
 
 return rcs
