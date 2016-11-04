7b0ac6d4c50e670428b5858fbb76806278c59120
Costin STROIE <cstroie@bitdefender.com>
2016-11-04 17:26:35 +0200

    Recoded some.

--- lcd.lua
+++ lcd.lua
@@ -7,9 +7,14 @@ local RS = 0x01  -- Register selector
 local EN = 0x04  -- Write Enable
 local BL = 0x08  -- BackLight
 
+local ROWS, COLS = 2, 16
+local LNADDR = {0x00, 0x40, 0x14, 0x54}
+local LNADDR = {0x80, 0xc0}
+
 function lcd:send(reg, data)
+  -- Send the bitstream
   -- reg: register 0 - instruction, 1 - data (default)
-  -- data: table
+  -- data: table of bytes to send
   local i, byte, ub, lb
   local stream = {}
   local bl = lcd_bl and BL or 0x00
@@ -30,48 +35,44 @@ function lcd:send(reg, data)
 end
 
 function lcd:light(onoff)
-  if onoff == "on" then
-    lcd_bl = true
-  else
-    lcd_bl = false
-  end
+  -- Turm on/off the LCD backlight
+  lcd_bl = onoff == "on" and true or false
   lcd:send(0, {0x00})
 end
 
 function lcd:cls()
+  -- Clear and go home
   lcd:send(0, {0x01})
 end
 
 function lcd:home()
+  -- Go home
   lcd:send(0, {0x02})
 end
 
 function lcd:cursor(onoff, style)
-  if (onoff == 0) then
-    lcd:send(0, {0x0c})
+  -- Set the cursor style
+  -- off:   cursor(false)
+  -- line:  cursor(true)
+  -- block: cursor(true, true)
+  if not onoff then
+    lcd:send(0, {0x08})
   else
-    if (style == 0) then
-      lcd:send(0, {0x0e})
+    if style then
+      lcd:send(0, {0x0d})
     else
-      lcd:send(0, {0x0f})
+      lcd:send(0, {0x0e})
     end
   end
 end
 
-function lcd:linecol(line, col)
-  if (line == 2) then
-    lcd:send(0, {0xc0 + bit.band(col, 0x0f)})
-  elseif (line == 1) then
-    lcd:send(0, {0x80 + bit.band(col, 0x0f)})
-  end
-end
-
 function lcd:write(text, line, col)
-  lcd:linecol(line, col)
-  if (type(text) == "table") then
+  -- Set the address and send the bytestream to write
+  lcd:send(0, {LNADDR[line] + col})
+  if type(text) == "table" then
     lcd:send(1, text)
   else
-    if (type(text) == "number") then text = tostring(text) end
+    text = (type(text) == "number") and tostring(text) or text
     local data = {}
     text:gsub(".", function(c) table.insert(data, c:byte()) end)
     lcd:send(1, data)
@@ -79,23 +80,29 @@ function lcd:write(text, line, col)
 end
 
 function lcd:writeline(text, line, align)
+  -- Write the specified line, aligned
   local col = 0
   if text ~= nil then
     if DEBUG then print("LCD" .. line .. ": " .. text) end
-    if align == "c" and #text < 16 then
-      col = (16 - #text) / 2
+    if align == "c" and #text < COLS then
+      col = (COLS - #text) / 2
+    elseif align == "r" and #text < COLS then
+      col = COLS - #text
     end
     lcd:write(text, line, col)
   end
 end
 
-function lcd:screen(line1, line2, align)
+function lcd:screen(line1, line2, align1, align2)
+  -- Write the two lines on the display, aligned
+  align2 = align2 or align1
   lcd:cls()
-  lcd:writeline(line1, 1, align)
-  lcd:writeline(line2, 2, align)
+  lcd:writeline(line1, 1, align1)
+  lcd:writeline(line2, 2, align2)
 end
 
 function lcd:defchar(num, data)
+  -- Define a big character
   -- num: char number 0-7
   -- data: table 8 lines x 5 bits
   lcd:send(0, {0x40 + 8 * num})
@@ -103,11 +110,13 @@ function lcd:defchar(num, data)
 end
 
 function lcd:bigwrite(uprow, btrow, col)
+  -- Write a big character at 'col'
   lcd:write(uprow, 1, col)
   lcd:write(btrow, 2, col)
 end
 
 function lcd:init()
+  -- Init the LCD (tested on 2x16)
   i2c.setup(lcd_id, lcd_sda, lcd_scl, i2c.SLOW)
   lcd:send(0, {0x33,0x32,0x28,0x0c,0x06})
 end
