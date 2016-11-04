eb516db81a374ff3b2ce8ff9b05614068a9cc890
Costin STROIE <cstroie@bitdefender.com>
2016-11-04 18:20:16 +0200

    Delay between repeats.

--- rcs.lua
+++ rcs.lua
@@ -27,6 +27,7 @@ function rcs:send(bits)
       table.insert(delays, LG)
     end
   end
+  table.insert(delays, 100000)
   gpio.mode(rcs_pin, gpio.OUTPUT)
   gpio.write(rcs_pin, gpio.LOW)
   gpio.serout(rcs_pin, gpio.HIGH, delays, rcs_count, 1)
