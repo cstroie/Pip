f5697159c3e99775d17106456284838b32db30d6
Costin STROIE <cstroie@bitdefender.com>
2016-11-04 17:27:26 +0200

    Better info.

--- wl.lua
+++ wl.lua
@@ -11,41 +11,43 @@ wl.check_counter = wl_tries
 wl.status = {"Idle", "Connecting", "Wrong password", "AP not found", "Failed", "Connected"}
 
 function wl:show()
+  -- Display WiFi data
   local result = false
   local ssid, password, bssid_set, bssid = wifi.sta.getconfig()
   local ip, nm, gw = wifi.sta.getip()
-  if (ssid ~= nil) and (ip ~= nil) then
+  if ssid and ip then
     lcd:screen(string.format("WiFi % 11s", ssid), string.format("% 16s", ip))
     result = true
   end
-  if DEBUG then print(wifi.sta.gethostname(), ip, nm, gw) end
+  debug(wifi.sta.gethostname(), ssid, wifi.sta.getrssi(), ip, nm, gw)
   return result
 end
 
 function wl:connect(ssid)
-  if DEBUG then print("Connecting to " .. ssid .. " ...") end
+  -- Try to connect and start the watchdog
   lcd:screen("WiFi connecting", "to " .. ssid)
   wifi.sta.config(ssid, wl_ap[ssid])
   tmr.start(1)
 end
 
 function wl.scan(lst)
+  -- Check the AP list for a known one
   local ap
-  if DEBUG then print("Scanning for WiFi hotspots...") end
   for ssid, v in pairs(lst) do
     authmode, rssi, bssid, channel = string.match(v, "(%d),(-?%d+),(%x%x:%x%x:%x%x:%x%x:%x%x:%x%x),(%d+)")
-    if DEBUG then print("  " .. ssid, rssi .. " dBm") end
-    if wl_ap[ssid] ~= nil then ap = ssid end
+    debug("  " .. ssid, rssi .. " dBm")
+    if wl_ap[ssid] then ap = ssid end
   end
-  if ap ~= nil then
+  if ap then
     wl:connect(ap)
   else
-    if DEBUG then print("No known WiFi.") end
+    debug("No known WiFi.")
     -- TODO SoftAP mode
   end
 end
 
 function wl:check()
+  -- Check the WiFi connectivity and try to identify an AP
   if wifi.sta.status() ~= 5 then
     if wl.check_counter > 0 then
       lcd:screen(string.format("WiFi wait... % 3d", wl.check_counter), wl.status[wifi.sta.status()])
@@ -65,6 +67,7 @@ function wl:check()
   end
 end
 
+-- Start the watchdog
 tmr.alarm(1, 1000, tmr.ALARM_SEMI, function() wl:check() end)
 
 return wl
