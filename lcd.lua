5ba7ada43ddb0b0e752fbd4b0061ce043ddaacd2
Costin STROIE <cstroie@bitdefender.com>
2016-12-23 18:00:59 +0200

    LCD module variant.

--- lcd.lua
+++ lcd.lua
@@ -8,46 +8,40 @@ local EN = 0x04  -- Write Enable
 local BL = 0x08  -- BackLight
 
 local ROWS, COLS = 2, 16
-local LNADDR = {0x00, 0x40, 0x14, 0x54}
 local LNADDR = {0x80, 0xc0}
 
-function lcd:send(reg, data)
+function lcd:send(reg, ...)
   -- Send the bitstream
   -- reg: register 0 - instruction, 1 - data (default)
   -- data: table of bytes to send
   local i, byte, ub, lb
-  local stream = {}
   local bl = lcd_bl and BL or 0x00
   if reg ~= 1 then reg = 0 end
   local rs = bit.band(RS, reg)
-  for i, byte in pairs(data) do
-    ub = bit.band(byte, 0xf0) + bl + rs
-    lb = bit.lshift(bit.band(byte, 0x0f), 4) + bl + rs
-    table.insert(stream, ub + EN)
-    table.insert(stream, ub)
-    table.insert(stream, lb + EN)
-    table.insert(stream, lb)
-  end
   i2c.start(lcd_id)
   i2c.address(lcd_id, lcd_dev, i2c.TRANSMITTER)
-  i2c.write(lcd_id, stream)
+  for i, byte in ipairs(arg) do
+    ub = bit.band(byte, 0xf0) + bl + rs
+    lb = bit.lshift(bit.band(byte, 0x0f), 4) + bl + rs
+    i2c.write(lcd_id, ub + EN, ub, lb + EN, lb)
+  end
   i2c.stop(lcd_id)
 end
 
 function lcd:light(onoff)
   -- Turm on/off the LCD backlight
   lcd_bl = onoff == "on" and true or false
-  lcd:send(0, {0x00})
+  lcd:send(0, 0x00)
 end
 
 function lcd:cls()
   -- Clear and go home
-  lcd:send(0, {0x01})
+  lcd:send(0, 0x01)
 end
 
 function lcd:home()
   -- Go home
-  lcd:send(0, {0x02})
+  lcd:send(0, 0x02)
 end
 
 function lcd:cursor(onoff, style)
@@ -56,26 +50,27 @@ function lcd:cursor(onoff, style)
   -- line:  cursor(true)
   -- block: cursor(true, true)
   if not onoff then
-    lcd:send(0, {0x08})
+    lcd:send(0, 0x08)
   else
     if style then
-      lcd:send(0, {0x0d})
+      lcd:send(0, 0x0d)
     else
-      lcd:send(0, {0x0e})
+      lcd:send(0, 0x0e)
     end
   end
 end
 
 function lcd:write(text, line, col)
   -- Set the address and send the bytestream to write
-  lcd:send(0, {LNADDR[line] + col})
+  lcd:send(0, LNADDR[line] + col)
   if type(text) == "table" then
+    lcd:send(1, unpack(text))
+  elseif type(text) == "number" then
     lcd:send(1, text)
   else
-    text = (type(text) == "number") and tostring(text) or text
     local data = {}
     text:gsub(".", function(c) table.insert(data, c:byte()) end)
-    lcd:send(1, data)
+    lcd:send(1, unpack(data))
   end
 end
 
@@ -107,8 +102,8 @@ function lcd:defchar(num, hexdata)
   -- data: table 8 lines x 5 bits
   local data = {}
   for i=1,8 do data[i] = tonumber(string.sub(hexdata, i+i-1, i+i), 16) end
-  lcd:send(0, {0x40 + 8 * num})
-  lcd:send(1, data)
+  lcd:send(0, 0x40 + 8 * num)
+  lcd:send(1, unpack(data))
 end
 
 function lcd:bigwrite(uprow, btrow, col)
@@ -120,7 +115,7 @@ end
 function lcd:init()
   -- Init the LCD (tested on 2x16)
   i2c.setup(lcd_id, lcd_sda, lcd_scl, i2c.SLOW)
-  lcd:send(0, {0x33,0x32,0x28,0x0c,0x06})
+  lcd:send(0, 0x33, 0x32, 0x28, 0x0c, 0x06)
 end
 
 return lcd
