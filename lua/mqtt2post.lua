cfbeb3ab4b8cb6316821b87557bf012af216a9a7
Costin STROIE <cstroie@bitdefender.com>
2016-12-27 20:05:02 +0200

    Added support for ubidots.

--- lua/mqtt2post.lua
+++ lua/mqtt2post.lua
@@ -9,10 +9,24 @@ dofile("/etc/keys-" .. pname)
 http = require("socket.http")
 thingspeak = require("thingspeak")
 stathat = require("stathat")
+ubidots = require("ubidots")
 
 -- Mosquitto
 local rstatus, rmodule = pcall(require, 'mosquitto')
 mosquitto = rstatus and rmodule or nil
+
+function int2float(x, pr)
+  -- Convert integer to "float" with specified precision
+  local result
+  if pr == 2 then
+    result = string.format("%d.%02d", math.floor(x/100), math.floor(x%100))
+  elseif pr == 3 then
+    result = string.format("%d.%03d", math.floor(x/1000), math.floor(x%1000))
+  else
+    result = string.format("%d.%02d", x, 0)
+  end
+  return result
+end
 
 -- MQTT
 if mosquitto ~= nil then
@@ -26,12 +40,6 @@ if mosquitto ~= nil then
     if     topic == "sensor/indoor/temperature" then
       thingspeak:collect("field1", payload)
       stathat.ez_value(STATHAT_KEY, "Indoor Temperature", payload)
-    elseif topic == "sensor/outdoor/temperature" then
-      local R = 98.2/(1024/payload - 1)
-      local T = 1/(1/298.15 + math.log(R/10.63)/3986)-273.15
-      thingspeak:collect("field6", payload)
-      thingspeak:collect("field7", T)
-      client:publish("sensor/outdoor/th", T)
     elseif topic == "sensor/indoor/humidity" then
       thingspeak:collect("field2", payload)
       stathat.ez_value(STATHAT_KEY, "Indoor Humidity", payload);
@@ -45,6 +53,28 @@ if mosquitto ~= nil then
       thingspeak:clear()
     elseif topic == "report/pop/uptime" then
       thingspeak:collect("status", "POP: " .. payload)
+
+    elseif topic == "sensor/outdoor/temperature" then
+      --local R = 98.2/(1024/payload - 1)
+      --local T = 1/(1/298.15 + math.log(R/10.63)/3986)-273.15
+      --thingspeak:collect("field6", payload)
+      --client:publish("sensor/outdoor/th", T)
+      thingspeak:collect("field7", T)
+      ubidots:collect("temperature", payload)
+    elseif topic == "sensor/outdoor/dewpoint" then
+      ubidots:collect("dewpoint", payload)
+    elseif topic == "sensor/outdoor/humidity" then
+      thingspeak:collect("field6", payload)
+      ubidots:collect("humidity", payload)
+    elseif topic == "sensor/outdoor/illuminance" then
+      thingspeak:collect("field8", payload)
+      ubidots:collect("illuminance", payload)
+    elseif topic == "sensor/outdoor/sealevel" then
+      ubidots:collect("pressure", payload)
+    elseif topic == "report/wxstation/vdd" then
+      ubidots:collect("vdd", int2float(payload, 3))
+      ubidots:post("wxstation", UBIDOTS_TOKEN)
+      ubidots:clear()
     end
   end
 
