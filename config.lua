f852d3838c111f03961b1682a6ff4a3b943ee5a3
Costin STROIE <cstroie@bitdefender.com>
2016-12-28 18:57:42 +0200

    Reconfigured.

--- config.lua
+++ config.lua
@@ -1,47 +1,33 @@
 -- Main configuration
+CFG = {}
+
+-- Global device data
+NODENAME = "NodeMCU"  -- device name
+TZ = 0                -- timezone
 
 -- WiFi
-wl_ap = {}
-wl_ap["ssid"] = "pass"
-wl_tries = 20
-
--- LCD
-lcd_id  = 0
-lcd_sda = 1      -- GPIO5
-lcd_scl = 2      -- GPIO4
-lcd_dev = 0x27   -- I2C Address
-lcd_bl  = true   -- Backlight
+CFG.WL = {}
+CFG.WL.tries = 20
+CFG.WL.AP = {}
+CFG.WL.AP["ssid"] = "pass"
 
 -- NTP server
-ntp_server = "0.europe.pool.ntp.org"
-ntp_interval = "765"
-timezone = 2
+CFG.NTP = {}
+CFG.NTP.server = "0.europe.pool.ntp.org"
+CFG.NTP.interval = 765
 
--- DHT pin
-dht11_pin = 4
-dht11_interval = "60"
-
--- WX
-wx_station = "ROXX0003"
+-- Thermistor
+CFG.TH = {}
+CFG.TH.interval = 60
 
 -- IoT MQTT
-iot_server = "m21.cloudmqtt.com"
-iot_port = 17223
-iot_id = "MQTT_ID"
-iot_user = "MQTT_USER"
-iot_pass = "MQTT_PASS"
-
--- Beep
-beep_pin = 10
-beep_pls = 500
-beep_rpt = 100
-
--- RCS
-rcs_pin = 3
-rcs_pl = 320
-rcs_bits = 24
-rcs_proto = 1
-rcs_count = 4
-rcs_dip = 0x1f
+CFG.IOT = {}
+CFG.IOT.server = "mqtt.example.com"
+CFG.IOT.port = 1883
+CFG.IOT.ssl = 0
+CFG.IOT.auto = 1
+CFG.IOT.id = "MQTT_ID"
+CFG.IOT.user = "MQTT_USER"
+CFG.IOT.pass = "MQTT_PASS"
 
 -- vim: set ft=lua ai ts=2 sts=2 et sw=2 sta nowrap nu :
