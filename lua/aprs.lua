40d39cd5b0808d1b79ef706c16eeac518c8e8c9b
Costin STROIE <cstroie@bitdefender.com>
2017-04-20 16:18:00 +0300

    Fixed APRS.

--- lua/aprs.lua
+++ lua/aprs.lua
@@ -2,9 +2,10 @@ module(..., package.seeall);
 
 local CALLSIGN = "FW0690"
 local PASSCODE = "-1"
-local LOCATION = "4427.43N/02607.59E"
+local LOCATION = "4427.67N/02608.03E"
 
-local APRS_SERVER = "cwop.aprs.net"
+local APRS_SERVER = "cwop5.aprs.net"
+--local APRS_SERVER = "euro.aprs2.net"
 local APRS_PORT = 14580
 
 
@@ -29,23 +30,30 @@ function aprs:post()
   local tfar = self.data["temp"] * 9 / 5 + 32
   local con = socket.connect(APRS_SERVER, APRS_PORT)
   if (con ~= nil) then
-    line = con:receive("*l")
+    --line = con:receive("*l")
     --print(line)
-    text = "user " .. CALLSIGN .. " pass " .. PASSCODE .. " vers WxIno 0.1"
+    text = "user " .. CALLSIGN .. " pass " .. PASSCODE .. " vers WxSta 20160311"
     con:send(text .. "\r\n")
-    line = con:receive("*l")
+    --line = con:receive("*l")
     --print(line)
     text = CALLSIGN .. ">APRS,TCPIP*:" ..
-           "@" .. os.date("!%d%H%M") .. "z" ..
+           "@" .. os.date("!%H%M%S") .. "h" ..
            LOCATION ..
            "_.../...g..." ..
            "t" .. string.format("%03d", math.floor(tfar)) ..
            "h" .. string.format("%02d",  math.floor(self.data["hmdt"])) ..
            "b" .. string.format("%05d",  math.floor(self.data["pres"] * 10)) ..
            "L" .. string.format("%03d",  math.floor(self.data["lux"] * 0.0079)) ..
-           "WxIno"
+           "WxSta-20160311-probe"
     con:send(text .. "\r\n")
     print(text)
+
+    -- Status: Zambretti forecaster
+    if self.data["zbrt"] then
+      text = CALLSIGN .. ">APRS,TCPIP*:>" .. os.date("%H:%M") .. " " .. self.data["zbrt"]
+      con:send(text .. "\r\n")
+      print(text)
+    end
 
     -- Telemetry http://www.aprs.net/vm/DOS/TELEMTRY.HTM
     -- "PARM.Battery,Charging/AC,GPS+Sat,A4,A5,A/C,Charging,GPS,B4,B5,B6,B7,B8", /* Channel names */
@@ -61,7 +69,7 @@ function aprs:post()
       text = CALLSIGN .. ">APRS,TCPIP*::" .. CALLSIGN .. "   :PARM.Vcc,RSSI,Heap,IRed,Vis,B1,B2,B3,B4,B5,B6,B7,B8"
       con:send(text .. "\r\n")
       print(text)
-      text = CALLSIGN .. ">APRS,TCPIP*::" .. CALLSIGN .. "   :EQNS.0,0.004,2.5,0,-1,0,0,200,0,0,255,0,0,255,0"
+      text = CALLSIGN .. ">APRS,TCPIP*::" .. CALLSIGN .. "   :EQNS.0,0.004,2.5,0,-1,0,0,200,0,0,256,0,0,256,0"
       con:send(text .. "\r\n")
       print(text)
       text = CALLSIGN .. ">APRS,TCPIP*::" .. CALLSIGN .. "   :UNIT.V,dBm,Bytes,units,units,N/A,N/A,N/A,N/A,N/A,N/A,N/A,N/A"
@@ -81,8 +89,8 @@ function aprs:post()
            string.format("%03d", math.floor((self.data["vcc"] - 2.5) * 250)) .. "," ..
            string.format("%03d", -self.data["rssi"]) .. "," ..
            string.format("%03d", math.floor(self.data["heap"] / 200)) .. "," ..
-           string.format("%03d", math.floor(self.data["visb"] / 255)) .. "," ..
-           string.format("%03d", math.floor(self.data["ired"] / 255)) .. "," ..
+           string.format("%03d", math.floor(self.data["visb"] / 256)) .. "," ..
+           string.format("%03d", math.floor(self.data["ired"] / 256)) .. "," ..
            "00000000"
     con:send(text .. "\r\n")
     print(text)
@@ -91,13 +99,6 @@ function aprs:post()
     -- TODO use a timer
     self.tmcnt = self.tmcnt + 1
     if self.tmcnt >= 999 then
-      self.tmcnt = 1
-    end
-    if self.tmcnt > self.stseq then
-      if self.tmcnt - self.stseq > 480 then
-        self.tmcnt = 1
-      end
-    elseif 999 + self.tmcnt - self.stseq > 480 then
       self.tmcnt = 1
     end
 
